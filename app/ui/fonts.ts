45f742ff32b1297d053dd41c4e6ff19d000f9f49
Flavio Patti <flaviogiovannipatti@gmail.com>
2025-06-22 17:14:38 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Roboto } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: ["400" ,"700"]
+});
+/* export const f= Roboto ricordare da inserire nel test */
